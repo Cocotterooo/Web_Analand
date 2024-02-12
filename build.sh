94f53b3d77a878ad278979b7786ddf9a3fe9107c
Cocotterooo <fcocoterooo@gmail.com>
2024-02-12 20:48:42 +0100

    docker_origenes permitidos

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
+API_URL=https://api.analand.net reflex export --backend-only
 reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,9 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Web_Analand",
+    app_name="Analand Super",
+    cors_allowed_origins=[ # Add the origins that are allowed to access the API
+        "http://localhost:3000",
+        "https://analand.net"
+    ],
 )
