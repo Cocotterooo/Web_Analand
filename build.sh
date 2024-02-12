9f7eb62e271614f03a2ece34eb66744ad6b679f2
Manuel Medina <103317717+Cocotterooo@users.noreply.github.com>
2024-02-12 21:42:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-reflex init
-API_URL=https://api.analand.net reflex export --backend-only
-reflex export --frontend-only
 rm -rf public
+reflex init
+API_URL=https://api.analand.net reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
