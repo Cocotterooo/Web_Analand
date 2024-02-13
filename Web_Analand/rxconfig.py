763a75b50e2182c71db76775628af6dad502d991
Cocotterooo <fcocoterooo@gmail.com>
2024-02-13 01:26:50 +0100

    corregido rxconfig para la compilación, nombre de la aplicación erroreno :/

--- Web_Analand/rxconfig.py
+++ Web_Analand/rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Analand Super",
+    app_name="Web_Analand",
     cors_allowed_origins=[ # Add the origins that are allowed to access the API
         "http://localhost:3000",
         "https://analand.net"
