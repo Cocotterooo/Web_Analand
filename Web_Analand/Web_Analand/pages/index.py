a38f5b02bbbdb730276ad23d67f8c6b7677856a9
Cocotterooo <fcocoterooo@gmail.com>
2024-02-14 20:13:31 +0100

    añadidos datos de pagina

--- Web_Analand/Web_Analand/pages/index.py
+++ Web_Analand/Web_Analand/pages/index.py
@@ -25,13 +25,13 @@ from Web_Analand.constants import DISCORD_GUILD_ID
         #self.discord_info = None
         #discord_info = discord("TOKEN", DISCORD_GUILD_ID)
 
-#@rx.page(
-#    title=utils.index_title,
-#    description=utils.index_description,
-#    image=utils.preview,
-#    #meta=utils.index_meta,
-#    on_load=[IndexState.get_discord_info]
-#)
+@rx.page(
+    title=utils.index_title,
+    description=utils.index_description,
+    image=utils.preview,
+    #meta=utils.index_meta,
+    #on_load=[IndexState.get_discord_info]
+)
 def index():
     return rx.vstack(
         # Background image with dark overlay style
