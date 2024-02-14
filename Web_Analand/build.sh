88a0659714479e1d37810c3e0130f8b7af8bbf3d
Manuel Medina <103317717+Cocotterooo@users.noreply.github.com>
2024-02-14 17:23:23 +0100

    Update build.sh

--- Web_Analand/build.sh
+++ Web_Analand/build.sh
@@ -5,7 +5,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://api.analand.net reflex export --frontend-only
+API_URL=https://analand-web.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
