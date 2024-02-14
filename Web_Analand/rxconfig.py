bd4bfe7656bceca887eed9ea941ceabaae9b66ec
Manuel Medina <103317717+Cocotterooo@users.noreply.github.com>
2024-02-14 17:29:24 +0100

    Update rxconfig.py

--- Web_Analand/rxconfig.py
+++ Web_Analand/rxconfig.py
@@ -4,7 +4,6 @@ config = rx.Config(
     app_name="Web_Analand",
     cors_allowed_origins=[ # Add the origins that are allowed to access the API
         "http://localhost:3000",
-        "https://analand.net",
-        "https://web-analand-kjfs9ce06-manuels-projects-f077ea90.vercel.app"
+        "https://analand.net"
     ],
 )
