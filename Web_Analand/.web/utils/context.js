a694e7697bbe0be813230f5fca1c100bb7eab78f
Cocotterooo <fcocoterooo@gmail.com>
2024-04-13 12:35:39 +0000

    Update public and app

--- Web_Analand/.web/utils/context.js
+++ Web_Analand/.web/utils/context.js
@@ -1,19 +1,16 @@
 import { createContext, useContext, useMemo, useReducer, useState } from "react"
 import { applyDelta, Event, hydrateClientStorage, useEventLoop, refs } from "/utils/state.js"
 
-<<<<<<< HEAD
-export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.index_state": {}, "state.update_vars_internal_state": {}, "state.on_load_internal_state": {}}
-=======
-export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.index_state": {}}
->>>>>>> 577110be3a097b7c877ead6efd224bbba9e35780
+export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.index_state": {}, "state.on_load_internal_state": {}, "state.update_vars_internal_state": {}}
 
+export const defaultColorMode = "light"
 export const ColorModeContext = createContext(null);
 export const UploadFilesContext = createContext(null);
 export const DispatchContext = createContext(null);
 export const StateContexts = {
   state: createContext(null),
-  state__on_load_internal_state: createContext(null),
   state__index_state: createContext(null),
+  state__on_load_internal_state: createContext(null),
   state__update_vars_internal_state: createContext(null),
 }
 export const EventLoopContext = createContext(null);
@@ -82,31 +79,29 @@ export function EventLoopProvider({ children }) {
 
 export function StateProvider({ children }) {
   const [state, dispatch_state] = useReducer(applyDelta, initialState["state"])
-  const [state__on_load_internal_state, dispatch_state__on_load_internal_state] = useReducer(applyDelta, initialState["state.on_load_internal_state"])
   const [state__index_state, dispatch_state__index_state] = useReducer(applyDelta, initialState["state.index_state"])
+  const [state__on_load_internal_state, dispatch_state__on_load_internal_state] = useReducer(applyDelta, initialState["state.on_load_internal_state"])
   const [state__update_vars_internal_state, dispatch_state__update_vars_internal_state] = useReducer(applyDelta, initialState["state.update_vars_internal_state"])
   const dispatchers = useMemo(() => {
     return {
       "state": dispatch_state,
-      "state.on_load_internal_state": dispatch_state__on_load_internal_state,
       "state.index_state": dispatch_state__index_state,
+      "state.on_load_internal_state": dispatch_state__on_load_internal_state,
       "state.update_vars_internal_state": dispatch_state__update_vars_internal_state,
     }
   }, [])
 
   return (
     <StateContexts.state.Provider value={ state }>
-    <StateContexts.state__on_load_internal_state.Provider value={ state__on_load_internal_state }>
     <StateContexts.state__index_state.Provider value={ state__index_state }>
+    <StateContexts.state__on_load_internal_state.Provider value={ state__on_load_internal_state }>
     <StateContexts.state__update_vars_internal_state.Provider value={ state__update_vars_internal_state }>
       <DispatchContext.Provider value={dispatchers}>
         {children}
       </DispatchContext.Provider>
-<<<<<<< HEAD
-    </StateContexts.state__on_load_internal_state.Provider>
     </StateContexts.state__update_vars_internal_state.Provider>
-    </StateContexts.state__index_state.Provider>
     </StateContexts.state__on_load_internal_state.Provider>
+    </StateContexts.state__index_state.Provider>
     </StateContexts.state.Provider>
   )
 }
