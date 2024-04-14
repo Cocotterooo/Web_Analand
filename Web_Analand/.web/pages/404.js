3a1aca2ee03db7c73af6d47da68ee2643088398f
Cocotterooo <fcocoterooo@gmail.com>
2024-04-14 10:52:15 +0000

    Update public and app

--- Web_Analand/.web/pages/404.js
+++ Web_Analand/.web/pages/404.js
@@ -14,7 +14,16 @@ import NextHead from "next/head"
 
 
 
-<<<<<<< HEAD
+const pulse = keyframes`
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+`
+
+
 export function Fragment_34f041f6aae0b1134e0b2a186d19d162 () {
   const [addEvents, connectErrors] = useContext(EventLoopContext);
 
@@ -44,17 +53,6 @@ export function Fragment_34f041f6aae0b1134e0b2a186d19d162 () {
 </Fragment>
   )
 }
-
-<<<<<<< HEAD
-const pulse = keyframes`
-    0% {
-        opacity: 0;
-    }
-    100% {
-        opacity: 1;
-    }
-`
-
 
 export function Fragment_3ea1a0318ded176a3888f943242093c6 () {
   const [addEvents, connectErrors] = useContext(EventLoopContext);
