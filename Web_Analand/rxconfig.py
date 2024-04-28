db9fe0dfe615b1af6488c39b53f6b800ffc7e6fe
Cocotterooo <fcocoterooo@gmail.com>
2024-04-28 17:59:03 +0200

    puerto del backend añadido

--- Web_Analand/rxconfig.py
+++ Web_Analand/rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name='Web_Analand',
+    backend_port=1502,
     cors_allowed_origins=[ # Add the origins that are allowed to access the API
         'http://localhost:3000',
         'https://analand.net',
